e8c7cc36a7d7c1e62bfbecab4b5b5283423652b2
Mark Archer <marka@ifit.com>
2019-02-07 23:46:58 -0800

    compile a file and return exports

---
+++ src/compileFile.ts
@@ -0,0 +1,34 @@
+
+import { parseHost } from './parse';
+import * as fs from 'fs';
+import { compileHost } from './compileJS';
+import { untick } from './common';
+
+const add = (...args) => args.reduce((m,a) => m + a)
+
+function setr(stack, obj, name, value) {
+  obj = obj.toString()
+  console.log({stack, obj, name, value})
+  const _setr = (obj, name, value) => obj[name] = value;
+  return ['`', _setr, obj, untick(name), value];
+}
+setr.isMacro = true;
+
+
+async function compileFile(file:string, options:any={}) {
+  const code = fs.readFileSync(file, 'utf8');
+  //console.log(code,'\n');
+  const ast = await parseHost([], code);
+  //console.log(JSON.stringify(ast, null, 2), '\n')
+  const exports:any = {}
+  const r = compileHost([{add,exports,setr}], ast);
+  // console.log(r.exec()('Mark'))
+  // console.log(r.exec()())
+  // console.log(exports)
+  // console.log(exports.greet('Blair'))
+  return exports
+}
+
+//compileFile('./src/host/parseParens.host').catch(console.error);
+compileFile('./src/host/greet.host').catch(console.error);
+
